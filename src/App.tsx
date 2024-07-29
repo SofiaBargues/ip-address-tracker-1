632d458016d7e9fd274061e047f44ff28a174d72
Sofia Bargues <barguessofia@gmail.com>
2024-07-29 11:40:13 +0100

    We have a puzzle map

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 
 function App() {
+  // TODO: 2. Geo position State
+  // const [count, setCount] = useState(0);
+
+  //  TODO: 3. useEffect > Fetch IP Position > save in state
+
   return (
     <>
-      <div className="bg-red-500 text-green-600">hello</div>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </>
+    // TODO: 1. Show Map with position from state
   );
 }
-
 export default App;
