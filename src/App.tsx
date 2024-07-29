67ba39a0e29eae70c4806316b408e18630c5cc7a
Sofia Bargues <barguessofia@gmail.com>
2024-07-29 14:25:24 +0100

    useEfect position

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
-import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { FormEvent, useCallback, useState } from "react";
 
 function App() {
-  // TODO: 2. Geo position State
-  // const [count, setCount] = useState(0);
+  const [position, setPosition] = useState<[number, number]>([51.505, -0.09]);
 
   //  TODO: 3. useEffect > Fetch IP Position > save in state
 
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    console.log(e);
+    // TODO: Fetch in handle submit
+  }
   return (
-    <>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-    </>
+    <section>
+      {/* --- (5) Add leaflet map container --- */}
+      <form onSubmit={handleSubmit}>
+        <input type="text" />
+        <button type="submit">{">"} </button>
+      </form>
+      <div>
+        <MapContainer
+          className="h-[300px] w-[300px]"
+          center={position}
+          zoom={6}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>{" "}
+    </section>
     // TODO: 1. Show Map with position from state
   );
 }
