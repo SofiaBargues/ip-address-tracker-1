e5f479afb86d2d82c680ddfe96597db2c64a1755
Sofia Bargues <barguessofia@gmail.com>
2024-07-30 07:37:59 +0100

    map

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { FormEvent, useCallback, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 
 function App() {
   const [position, setPosition] = useState<[number, number]>([51.505, -0.09]);
-
   //  TODO: 3. useEffect > Fetch IP Position > save in state
-
+  useEffect(() => {
+    fetch(
+      "https://geo.ipify.org/api/v2/country,city?apiKey=at_6Th4tJ6FGawlQdq2YjUIHzpHHtDzP&ipAddress=8.8.8.8"
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setPosition([data.location.lat, data.location.lng]);
+      });
+  }, []);
   async function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
     console.log(e);
     // TODO: Fetch in handle submit
   }
+  console.log(position);
   return (
     <div className="w-[325px] h-screen bg-slate-700 justify-center">
       <img
@@ -27,6 +37,7 @@ function App() {
       <div>
         <MapContainer
           className="h-[500px] w-[325px]"
+          key={JSON.stringify(position)}
           center={position}
           zoom={6}
           scrollWheelZoom={false}
